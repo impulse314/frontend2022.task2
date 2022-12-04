d140eb1135c04bbb3a843ad795d81f7c66c6632d
RamuchiRam <ramuchiram@gmail.com>
2022-12-04 18:04:07 +0700

    Исправлен модификатор --active

--- src/js/main.js
+++ src/js/main.js
@@ -146,8 +146,16 @@ const faq = document.querySelectorAll(".faq-page__question");
 let i;
 for (i = 0; i < faq.length; i++) {
     faq[i].addEventListener("click", function () {
-
-        this.classList.toggle("--active");
+      
+      if (this.classList.contains("faq-page__question")) {
+        this.classList.remove("faq-page__question")
+        this.classList.add("faq-page__question--active")
+      }
+      else if (this.classList.contains("faq-page__question--active")) {
+        this.classList.remove("faq-page__question--active")
+        this.classList.add("faq-page__question")
+      }
+        // this.classList.toggle("--active");
 
         let body = this.nextElementSibling;
         if (body.style.display === "block") {
